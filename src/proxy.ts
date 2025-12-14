213a4b9e8e1435b66c7d2e91d584d1f1ebe35033
Piotr Papież <piotr.a.papiez@gmail.com>
2025-12-14 19:47:42 +0100

    Proxy fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,13 +6,16 @@ import type { NextRequest } from "next/server";
 
 export function proxy(req: NextRequest): NextResponse {
     const accessToken = req.cookies.get("accessToken")?.value;
+    const refreshToken = req.cookies.get("refreshToken")?.value;
     const { pathname } = req.nextUrl;
 
     const isAuthRoute = pathname.startsWith("/auth");
     const isAppRoute = pathname.startsWith("/tasks");
 
-    if (isAuthRoute && accessToken) return NextResponse.redirect(new URL("/tasks", req.url));
-    if (isAppRoute && !accessToken) return NextResponse.redirect(new URL("/auth/login", req.url));
+    if (isAuthRoute && (accessToken || refreshToken)) return NextResponse.redirect(new URL("/tasks", req.url));
+
+    // if (isAuthRoute && accessToken) return NextResponse.redirect(new URL("/tasks", req.url));
+    // if (isAppRoute && !accessToken) return NextResponse.redirect(new URL("/auth/login", req.url));
 
     return NextResponse.next();
 }
